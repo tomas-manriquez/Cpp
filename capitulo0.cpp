89362445588c6387416ad5e3650c5df336fc71ca
Tomas Manriquez <tomas.manriquez@usach.cl>
2023-12-22 20:57:25 -0300

    ejercicio 0-10

---
+++ capitulo0.cpp
@@ -0,0 +1,11 @@
+//int main(){return 0;} programa mas corto que se me puede ocurrir
+#include <iostream>
+
+int main()
+{
+    std::cout<<"#include\n<iostream>"<<std::endl;
+    std::cout<<"int\nmain(){"<<std::endl;
+    std::cout<<"std::cout<<\"Hello\nWorld!\"<<std::endl;"<<std::endl;
+    std::cout<<"return\n0;"<<std::endl;
+    std::cout<<"}"<<std::endl;
+}
